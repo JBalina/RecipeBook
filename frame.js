e1410f7e16029fa6fda65ac79c6e4036cd9bebf1
Jonathan Balina <jonathan_balina@yahoo.com>
2021-01-21 03:54:07 -0500

    Added hyperlink to title

--- frame.js
+++ frame.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
 	var title = document.createElement("h1")
-	title.appendChild(document.createTextNode("The Recipe Book"))
+	var link = document.createElement("a")
+	link.appendChild(document.createTextNode("The Recipe Book"))
+	link.href = "https://jbalina.github.io/RecipeBook/"
+	title.appendChild(link)
 	title.id = "title"
 	var element = document.getElementById("header")
 	element.appendChild(title)
